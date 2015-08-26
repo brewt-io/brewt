79c8aae8042f8aaa9feb9b73ac97751ffb5d5748
Greg M <daedalus12@gmail.com>
2015-08-26 13:53:16 -0700

    Started definition of basic ingredient classes

--- ingredients.js
+++ ingredients.js
@@ -1,3 +1,35 @@
-/**
- * Created by meessg on 2015.08.26.
- */
+
+function Ingredient(name){
+    this.name = name;
+}
+
+function Grain(name, yieldPer, color, amount){
+    Ingredient.call(this, name);
+    this.yieldPer = yieldPer;
+    this.color = color;
+    this.amount = amount;
+}
+
+function Hops(name, alphaAcids, amount, time){
+    Ingredient.call(this, name);
+    this.alphaAcids = alphaAcids;
+    this.amount = amount;
+    this.time = time;
+}
+
+function Yeast(name, attenuation){
+    Ingredient.call(this, name);
+    this.attenuation = attenuation;
+}
+
+Grain.prototype = Object.create(Ingredient.prototype);
+Hops.prototype = Object.create(Ingredient.prototype);
+Yeast.prototype = Object.create(Ingredient.prototype);
+
+g = new Grain("Pale 2-Row (US)", 0.80, 1.8, 10);
+h = new Hops("Cascade", .068, 2, 60);
+y = new Yeast("San Diego Super Yeast", 0.795);
+
+console.log(g);
+console.log(h);
+console.log(y);
