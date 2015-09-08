044d193b2c8349d746c184a6159093d29eb82237
Greg M <daedalus12@gmail.com>
2015-09-08 11:15:18 -0700

    Copied css and html from codepen, added basic list population.

--- ingredients.js
+++ ingredients.js
@@ -149,3 +149,23 @@ console.log("Final Gravity:    " + r.calculateFinalGravity().toFixed(3));
 console.log("ABV:              " + r.calculateABV().toFixed(1) + "%");
 console.log("Color:            " + r.calculateColor().toFixed(0) + " (SRM)");
 console.log("Bitterness:       " + r.calculateBitterness().toFixed(0) + " (IBU)");
+
+
+$(document).ready( function(){
+    fermentablesList = $("#fermentables-list-content").find("> ul");
+    yeastList = $("#yeast-list-content").find("> ul");
+    hopsList = $("#hops-list-content").find("> ul");
+    var i;
+    for (i = 0; i<grains.length; i++){
+        fermentablesList.append('<li>' + grains[i].name + '</li>');
+    }
+
+    for (i = 0; i<hops.length; i++){
+        hopsList.append('<li>' + hops[i].name + '</li>');
+    }
+
+    for (i = 0; i<yeast.length; i++){
+        yeastList.append('<li>' + yeast[i].name + '</li>');
+    }
+
+});
