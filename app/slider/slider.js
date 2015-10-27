b5f3d79aea088bb332eb051b265179787d40984d
Murray Clement <contact@crtv.me>
2015-10-27 14:48:33 -0700

    Added newest slider code

--- app/slider/slider.js
+++ app/slider/slider.js
@@ -11,3 +11,25 @@ angular.module('brewt').directive('slider', function(){
         templateUrl: 'app/slider/slider.html'
     }
 });
+
+// INJECTED CODE //
+
+// These are the vars
+
+var left = 25; // min
+var current = 65.5; // current tooltip
+var right = 75; // max
+
+var range = right - left; // range value
+
+// This does the things
+
+$(".btn1").click(function() {
+  $(".left").text(left).css("left", "calc(" + left + "% - .75em)");
+  $(".current").text(current).css("left", "calc(" + current + "% - 1.5%)");
+  $(".right").text(right).css("left", "calc(" + right + "% - 1.25em)");
+
+  $(".range").css("left", left + "%").css("width", range +'%');
+});
+
+// Maybe I'll do more things
