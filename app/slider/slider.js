b9f280fd64e720181c2f873e8e675aa783e67b62
Daedalus12 <Daedalus12@gmail.com>
2015-10-28 22:26:38 -0700

    Trying to get slider hooked up

--- app/slider/slider.js
+++ app/slider/slider.js
@@ -8,28 +8,28 @@ angular.module('brewt').directive('slider', function(){
             styleMax:"=",
             val:"="
         },
-        templateUrl: 'app/slider/slider.html'
+        templateUrl: 'app/slider/slider.html',
+        link:function(scope, elem){
+            elem.find(".current").text = scope.val; //.css("left", "calc(" + current + "% - 1.5%)");
+            elem.find(".left").text = scope.styleMin;
+            elem.find(".right").text = scope.styleMax;
+
+            console.log(elem.find(".right"));
+
+            //elem.find(".left").css["left"] = "calc(" + scope.styleMin/(scope.propMax - scope.propMin) + "% - .75em)" ;
+            //elem.find(".right").css["left"] = "calc(" + scope.styleMax/(scope.propMax - scope.propMin) + "% - .75em)";
+            //elem.find(".left").css("left", "10%" );
+            //elem.find(".right").css("left", "78%");
+
+            //console.log(elem.find(".left"));
+            //elem.find(".right").css = {"left": scope.styleMax/(scope.propMax - scope.propMin) + "%"};
+            //elem.find(".range")
+
+            //elem[".btn1"].click(function () {
+            //    elem(".left").text(left).css("left", );
+            //    elem(".right").text(right).css("left", "calc(" + right + "% - 1.25em)");
+            //    elem(".range").css("left", left + "%").css("width", range + '%');
+            //});
+        }
     }
 });
-
-// INJECTED CODE //
-
-// These are the vars
-
-var left = 25; // min
-var current = 65.5; // current tooltip
-var right = 75; // max
-
-var range = right - left; // range value
-
-// This does the things
-
-$(".btn1").click(function() {
-  $(".left").text(left).css("left", "calc(" + left + "% - .75em)");
-  $(".current").text(current).css("left", "calc(" + current + "% - 1.5%)");
-  $(".right").text(right).css("left", "calc(" + right + "% - 1.25em)");
-
-  $(".range").css("left", left + "%").css("width", range +'%');
-});
-
-// Maybe I'll do more things
