9297618ca9894d1fea301d5220f20f9605ab25cf
Greg M <Daedalus12@gmail.com>
2015-10-28 23:26:39 -0700

    Got sliders updating on button click

--- app/slider/slider.js
+++ app/slider/slider.js
@@ -9,27 +9,18 @@ angular.module('brewt').directive('slider', function(){
             val:"="
         },
         templateUrl: 'app/slider/slider.html',
-        link:function(scope, elem){
-            elem.find(".current").text = scope.val; //.css("left", "calc(" + current + "% - 1.5%)");
-            elem.find(".left").text = scope.styleMin;
-            elem.find(".right").text = scope.styleMax;
-
-            console.log(elem.find(".right"));
-
-            //elem.find(".left").css["left"] = "calc(" + scope.styleMin/(scope.propMax - scope.propMin) + "% - .75em)" ;
-            //elem.find(".right").css["left"] = "calc(" + scope.styleMax/(scope.propMax - scope.propMin) + "% - .75em)";
-            //elem.find(".left").css("left", "10%" );
-            //elem.find(".right").css("left", "78%");
-
-            //console.log(elem.find(".left"));
-            //elem.find(".right").css = {"left": scope.styleMax/(scope.propMax - scope.propMin) + "%"};
-            //elem.find(".range")
-
-            //elem[".btn1"].click(function () {
-            //    elem(".left").text(left).css("left", );
-            //    elem(".right").text(right).css("left", "calc(" + right + "% - 1.25em)");
-            //    elem(".range").css("left", left + "%").css("width", range + '%');
-            //});
-        }
+        controller:function($scope){
+            this.update= function(){
+                var scale = $scope.propMax - $scope.propMin;
+                $scope.leftStyle = {'left': 'calc(' + 100*($scope.styleMin - $scope.propMin)/scale+'% - 1.25em)'};
+                $scope.rightStyle = {'left': 'calc(' + 100*($scope.styleMax - $scope.propMin)/scale+'% - 1.25em)'};
+                $scope.currentStyle = {'left': 100*($scope.val - $scope.propMin)/scale+'%'};
+                $scope.rangeStyle = {
+                    'left': 100*($scope.styleMin - $scope.propMin)/scale+'%',
+                    'width': 100*($scope.styleMax - $scope.styleMin)/scale+'%'
+                };
+            }
+        },
+        controllerAs: 'sliderCtrl'
     }
 });
