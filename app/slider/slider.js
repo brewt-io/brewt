3403635d79e079489da5313cacbffe0beca57bd7
Daedalus12 <Daedalus12@gmail.com>
2015-11-01 18:16:54 -0800

    Got sliders working dynamically!

--- app/slider/slider.js
+++ app/slider/slider.js
@@ -6,20 +6,30 @@ angular.module('brewt').directive('slider', function(){
             propMax:"=",
             styleMin:"=",
             styleMax:"=",
-            val:"="
+            recipeVal:"="
         },
         templateUrl: 'app/slider/slider.html',
         controller:function($scope){
-            this.update= function(){
-                var scale = $scope.propMax - $scope.propMin;
+
+            var scale = $scope.propMax - $scope.propMin;
+
+            var updateStyle = function(){
                 $scope.leftStyle = {'left': 'calc(' + 100*($scope.styleMin - $scope.propMin)/scale+'% - 1.25em)'};
                 $scope.rightStyle = {'left': 'calc(' + 100*($scope.styleMax - $scope.propMin)/scale+'% - 1.25em)'};
-                $scope.currentStyle = {'left': 100*($scope.val - $scope.propMin)/scale+'%'};
                 $scope.rangeStyle = {
-                    'left': 100*($scope.styleMin - $scope.propMin)/scale+'%',
-                    'width': 100*($scope.styleMax - $scope.styleMin)/scale+'%'
+                    'left': 100 * ($scope.styleMin - $scope.propMin) / scale + '%',
+                    'width': 100 * ($scope.styleMax - $scope.styleMin) / scale + '%'
                 };
-            }
+            };
+
+            var updateVal = function(){
+                $scope.currentStyle = {'left': 100*($scope.recipeVal- $scope.propMin)/scale+'%'};
+
+            };
+
+            $scope.$watch(function(){return $scope.styleMin;}, function(){updateStyle()});
+            $scope.$watch(function(){return $scope.styleMax;}, function(){updateStyle()});
+            $scope.$watch(function(){return $scope.recipeVal;}, function(){updateVal()});
         },
         controllerAs: 'sliderCtrl'
     }
